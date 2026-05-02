6fe651219f06609e894d33ebcf8a20c643a78c01
Mohammad Noyeem uddin sifat <107163779+sif69@users.noreply.github.com>
2026-05-02 14:38:59 +0600

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+
+    int climbway(int n, vector<int>&dp){
+
+        if(n < 0) return 0;
+        if(n == 0) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = climbway(n-1,dp) + climbway(n-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return climbway(n,dp);
+    }
+};
